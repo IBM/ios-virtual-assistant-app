d1b9cd703092a624809f81b2b3eb8590a579f333
IBM Developer Experience <devrtdo@us.ibm.com>
2019-01-22 20:40:16 +0000

    Automated Update

--- ioswatsonassistantexample/AppDelegate.swift
+++ ioswatsonassistantexample/AppDelegate.swift
@@ -26,7 +26,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 
         let myBMSClient = BMSClient.sharedInstance
@@ -66,7 +65,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
 
 }
 
--- ioswatsonassistantexample/AssistantError.swift
+++ ioswatsonassistantexample/AssistantError.swift
@@ -18,15 +18,17 @@ import Foundation
 
 // Custom error set
 enum AssistantError: Error, CustomStringConvertible {
-    
+
     case invalidCredentials
 
     case missingCredentialsPlist
 
     case missingAssistantCredentials
-    
+
     case noWorkspacesAvailable
-    
+
+    case noData
+
     case error(String)
 
     case noWorkspaceId
@@ -38,6 +40,7 @@ enum AssistantError: Error, CustomStringConvertible {
         case .missingAssistantCredentials: return "Missing Watson Assistant Credentials"
         case .noWorkspacesAvailable: return "No Workspaces Available"
         case .noWorkspaceId: return "No Workspaces Id Provided"
+        case .noData: return "Bad Response"
         case .error: return "An Error Occurred"
         }
     }
@@ -49,10 +52,11 @@ enum AssistantError: Error, CustomStringConvertible {
         case .missingAssistantCredentials: return "Make sure to follow the steps in the README to create the credentials file."
         case .noWorkspacesAvailable: return "Be sure to set up a Watson Assistant workspace from the IBM Cloud dashboard."
         case .noWorkspaceId: return "Be sure to set up a Watson Assistant workspace from the IBM Cloud dashboard."
+        case .noData: return "No Watson Assistant data was received."
         case .error(let msg): return msg
         }
     }
-    
+
     var description: String {
         return self.alertTitle + ": " + self.alertMessage
     }
